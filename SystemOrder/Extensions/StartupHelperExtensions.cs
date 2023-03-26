918dc3f22a8c8bd96bf3de1ef249c9339d5b307f
ŽIŠKA Michal <Ziska@Patria.cz>
2023-03-26 18:28:44 +0200

    program, uprava configureServices/pipelines.

---
+++ SystemOrder/Extensions/StartupHelperExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using SystemOrder.Domain.Repositories;
+using SystemOrder.Domain.Services;
+using SystemOrder.Persistence.Context;
+using SystemOrder.Persistence.Repositories;
+using SystemOrder.Services;
+
+namespace SystemOrder.Extensions
+{
+    public static class StartupHelperExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<IOrderService, OrderService>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            builder.Services.AddDbContext<OrderDbContext>(
+                opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("OrderDbContext")));
+
+            return builder.Build();
+        }
+
+        public static async Task<WebApplication> ConfigurePipeline(this WebApplication app)
+        {
+            await EnsureDatabaseIsMigrated(app.Services);
+
+            async Task EnsureDatabaseIsMigrated(IServiceProvider services)
+            {
+                using var scope = services.CreateScope();
+                using var ctx = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+                if (ctx is not null)
+                {
+                    //await SeedData.Seed(ctx);
+                    await ctx.Database.MigrateAsync();
+                }
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
